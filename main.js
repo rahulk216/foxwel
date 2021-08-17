7396becedace17f3f2d4848e040540b3b05d7aff
Rahul Kakkadan <rahulkakkadan216@gmail.com>
2021-08-17 14:40:28 +0530

    'social'

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
- 
+document.addEventListener('DOMContentLoaded', function () {
+	if (document.getElementById('gallery-slider') != null) {
+		new Splide('#gallery-slider', {
+			width: '100%',
+			arrows: false,
+			autoplay: true,
+			infinite: true,
+		}).mount();
+	}
+	if (document.getElementById('team-slider') != null) {
+		new Splide('#team-slider', {
+			width: '100%',
+			arrows: false,
+			autoplay: true,
+			infinite: true,
+		}).mount();
+	}
+});
