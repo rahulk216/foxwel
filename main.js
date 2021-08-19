384cb0735f52b5bfc364b832abd65b28bb1ee8ec
Rahul Kakkadan <rahulkakkadan216@gmail.com>
2021-08-19 10:12:03 +0530

    businessaboutus

--- main.js
+++ main.js
@@ -16,3 +16,21 @@ document.addEventListener('DOMContentLoaded', function () {
 		}).mount();
 	}
 });
+
+if(document.getElementById("business-brandPopup")!=null)
+{
+  const modal=document.getElementById("business-brandPopup");
+  const modal_title=document.getElementById("business-brandPopup-title");
+  const modal_content=document.getElementById("business-brandPopup-body");
+  function openModal(elem) {
+    modal.style.width = "100%";
+    modal_title.innerText = elem.dataset.title;
+    modal_content.innerText = elem.dataset.content;
+    body.classList.add('overflow-y-hidden')
+  }
+  
+  function closePopup() {
+    modal.style.width = "0%";
+    body.classList.remove('overflow-y-hidden')
+  }
+}
