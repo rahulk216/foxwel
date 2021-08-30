b56ff7495fe1dd20024f7efaabfeebd99ca32176
Rahul Kakkadan <rahulkakkadan216@gmail.com>
2021-08-30 13:39:35 +0530

    variable text

--- main.js
+++ main.js
@@ -15,22 +15,25 @@ const close_link = document.querySelector('.close')
 
 document.addEventListener('DOMContentLoaded', function () {
   	if (document.getElementById('gallery-slider-left') != null) {
-    var sec =	new Splide('#gallery-slider', {
+    var sec =	new Splide('#gallery-slider-left', {
 			width: '100%',
 			arrows: false,
 			autoplay: true,
 			infinite: true,
-      pagination:false
+      pagination:false,
 		}).mount(); 
-	}
-	 if (document.getElementById('gallery-slider') != null) {
-     	var primary = new Splide('#gallery-slider-left', {
+
+    var primary = new Splide('#gallery-slider', {
 			width: '100%',
 			autoplay: true,
       arrows: false,
 			infinite: true,
-      pagination:false
-		}).mount();
+		});
+
+    primary.sync(sec).mount();
+	}
+	 if (document.getElementById('gallery-slider') != null) {
+     
    }
 	if (document.getElementById('team-slider') != null) {
 		new Splide('#team-slider', {
